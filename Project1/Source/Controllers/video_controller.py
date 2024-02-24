c0a020094dfe44881b4a1952f5e1d9a63044318d
gorkemozbay <gorkemozbay0405@gmail.com>
2024-02-24 23:29:44 +0300

    Line draw is added.

--- Project1/Source/Controllers/video_controller.py
+++ Project1/Source/Controllers/video_controller.py
@@ -1,5 +1,6 @@
 import cv2
 import Utils.project_settings as ps
+from   Utils.data_classes import Line, Point
 
 class VideoController():
     
@@ -7,17 +8,32 @@ class VideoController():
         print("VideoController is created")
         self.cap = cv2.VideoCapture(source)
         self.model = model
-        self.current_frame = None
+        # Move the fields below somewhere else
         self.line_list = []
-        
-                
-    def draw_line(frame, point_1, point_2, color, thickness):
-        cv2.line(frame, (point_1.x, point_1.y), (point_2.x, point_2.y), color, thickness)
+        self.point1 = None
+        self.point2 = None
+        self.point_count = 0
+
+
+    def get_mouse_position(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.point_count += 1
+            if self.point_count %2 == 1:
+                self.point1 = Point(x, y)
+            else:
+                self.point2 = Point(x, y)
+                line = Line(self.point1, self.point2, ps.RED, ps.LINE_THICKNESS)
+                self.line_list.append(line)
+
+
+    def draw_line(self, frame, line):
+        cv2.line(frame, (line.point1.x, line.point1.y), (line.point2.x, line.point2.y), 
+                 line.color, line.thickness)
         return frame
-    
-    
+
+
     def get_YOLO_results(self, frame):
-        results = self.model(frame, classes=[1,2,3,7], show=False)
+        results = self.model(frame, classes=[1,2,3,7], show=False, verbose=False)
         return results
 
 
@@ -28,25 +44,29 @@ class VideoController():
             for xyxy in xyxys:
                 cv2.rectangle(frame, (int(xyxy[0]), int(xyxy[1])), (int(xyxy[2]), int(xyxy[3])), ps.GREEN, ps.BBOX_THICKNESS)
         return frame
-    
-    
-    def add_lines(self):
+
+
+    def add_lines(self, frame):
         for line in self.line_list:
-            pass
-    
-    
+            self.draw_line(frame, line)
+        return frame
+
+
     def run(self):
         
         if not self.cap.isOpened():
             print("Failed to open source")
             exit()
         
+        cv2.namedWindow("out")
+        cv2.setMouseCallback("out", self.get_mouse_position)
         while True:
             success, frame = self.cap.read()
             if success:
                 results = self.get_YOLO_results(frame)
                 annotated_frame = self.annotate_frame(results, frame)
-                cv2.imshow('out', annotated_frame)
+                annotated_frame = self.add_lines(annotated_frame)
+                cv2.imshow("out", annotated_frame)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
             else:
--- Project1/Source/Utils/data_classes.py
+++ Project1/Source/Utils/data_classes.py
@@ -4,3 +4,11 @@ from dataclasses import dataclass
 class Point:
     x: int
     y: int
+
+    
+@dataclass
+class Line:
+    point1: Point
+    point2: Point
+    color: tuple
+    thickness: int
