74011a64d82c75500d36c51ac56d405215d4be19
gorkemozbay <gorkemozbay0405@gmail.com>
2024-02-24 14:21:26 +0300

    Video process added.

--- Project1/Source/main.py
+++ Project1/Source/main.py
@@ -3,17 +3,21 @@ import cv2
 from   ultralytics import YOLO 
 
 TEST_IMAGE = r'Images\bus.jpg'
+TEST_VIDEO = r'Videos\cars.mp4'
 MODEL      = r'Models\yolov8n.pt'
 
 def process_video(model):
-    test_image_path = os.path.join(os.getcwd(), TEST_IMAGE)
-    results = model(test_image_path, save = True)
+    source_path = os.path.join(os.getcwd(), TEST_VIDEO)
+    results = model(source_path, classes=[1,2,3,7] ,show=True)
+
 
 def main():
-    model_path = os.path.join(os.getcwd(), MODEL)
+    model_path = os.path.join(os.getcwd(), MODEL)    
+    print(model_path)
     model = YOLO(model_path)
+    
     process_video(model)
-
-
+    
+    
 if __name__ == "__main__":
     main()
